1a60dafe617df479cf36c67e35b97e0e089bb727
Kecen Yao <ky2138@nyu.edu>
2023-04-25 10:07:04 -0400

    Final deploy

--- test.mjs
+++ test.mjs
@@ -1,4 +1,5 @@
 import {Builder, By, Key, until} from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome.js';
 import assert from 'assert';
 
 describe('User Login Test', function() {
@@ -36,37 +37,13 @@ describe('User Login Test', function() {
       await driver.manage().setTimeouts({implicit: 5000});
       assert.equal(await driver.getCurrentUrl(), 'http://localhost:3000/');
     });
-});
 
-describe('User Post Test', function() {
-  let driver;
-
-  before(async function() {
-    driver = await new Builder().forBrowser('chrome').build();
-    await driver.get('http://localhost:3000/post/add');
-  });
-
-  after(async function() {
-    await driver.quit();
-  });
-
-  it('should go to the login page', async function() {
-      assert.equal(await driver.getCurrentUrl(), 'http://localhost:3000/login');
-      const usernameText = await driver.findElement(By.id('username'));
-      const passwordText = await driver.findElement(By.id('password'));
-      const loginBtn = await driver.findElement(By.id('LoginBtn'));
-      await usernameText.sendKeys('ky2138');
-      await passwordText.sendKeys('123');
-      await loginBtn.click();
-      await driver.manage().setTimeouts({implicit: 5000});
-  });
-
-  it('should go to the post page', async function() {
+  it('should logout', async function() {
     assert.equal(await driver.getCurrentUrl(), 'http://localhost:3000/');
-    const newPostLink = await driver.findElement(By.id('newPost'));
+    const LogoutLink = await driver.findElement(By.id('logout-form'));
     await driver.manage().setTimeouts({implicit: 5000});
-    await newPostLink.click();
+    await LogoutLink.click();
     await driver.manage().setTimeouts({implicit: 5000});
-    // assert.equal(await driver.getCurrentUrl(), 'http://localhost:3000/post/add');
+    assert.equal(await driver.getCurrentUrl(), 'http://localhost:3000/');
   });
 });
